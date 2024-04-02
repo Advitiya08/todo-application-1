49ad1ea0c8a8ab3fb4e460edd031f7a0b480a91c
advitiya.srivastava <sri.advitiya@gmail.com>
2024-04-03 02:37:21 +0530

    format

--- src/lib/TodoItem.tsx
+++ src/lib/TodoItem.tsx
@@ -1,29 +1,29 @@
 import { Todo } from '@/Interface/Todo'
 import { Checkbox } from '@/components/ui/checkbox'
-import {FC, useState} from 'react'
-interface TodoItemProps{
-    todoItem:Todo,
-    checkBoxModifed?:Function
+import { FC, useState } from 'react'
+interface TodoItemProps {
+    todoItem: Todo,
+    checkBoxModifed?: Function
 }
-const TodoItem:FC<TodoItemProps>=function({todoItem,checkBoxModifed}){
-       // state
+const TodoItem: FC<TodoItemProps> = function ({ todoItem, checkBoxModifed }) {
+    // state
     //    const [checked,setChecked]=useState(todoItem.isCompleted)
-       const onchangeCheckBox=function(value:any){
-           console.log(value,todoItem.id)
-            todoItem.isCompleted=value
-            if(checkBoxModifed){
-                checkBoxModifed(todoItem.id,value)
-            }
+    const onchangeCheckBox = function (value: any) {
+        console.log(value, todoItem.id)
+        todoItem.isCompleted = value
+        if (checkBoxModifed) {
+            checkBoxModifed(todoItem.id, value)
         }
-    return(
+    }
+    return (
         <div className='flex items-center gap-2'>
-          <Checkbox checked={todoItem.isCompleted} onCheckedChange={(value)=>onchangeCheckBox(value)}></Checkbox>
-        <p className={todoItem.isCompleted?"line-through":""}>
-            {todoItem.title}
-        </p>
+            <Checkbox checked={todoItem.isCompleted} onCheckedChange={(value) => onchangeCheckBox(value)}></Checkbox>
+            <p className={todoItem.isCompleted ? "line-through" : ""}>
+                {todoItem.title}
+            </p>
 
         </div>
     )
-        
+
 }
 export default TodoItem;
